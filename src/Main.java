7fbb9da3f155f98065e46dcd022627e954029d47
Christian Beckham <urenacb22@gmail.com>
2022-11-09 15:23:28 -0600

    feat: instantiated Robot and Weapon class for dependency injection task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+//        Weapon sword = new Weapon("Sword", 50);
+        Weapon axe = new Weapon("axe", 80);
+        Robot robot_one = new Robot(axe);
+        System.out.println(robot_one.name);
+        robot_one.attack();
     }
 }
